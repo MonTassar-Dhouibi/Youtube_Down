d539fc200c391eb8ade1cdf04f6d9d82bfcb13c0
VËndîtta0VËN0M <72995093+VEnditta0Xx@users.noreply.github.com>
2022-04-17 04:58:51 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,10 +1,10 @@
 import os
 print("#"*20,"\n\n")
-print("{}^20".format("InSTTALLER"))
+print("{:_^20s}".format("InSTTALLER"))
 print("\n\n","#"*20)
 print("Installing ....")
 os.system("mv bin $HOME")
 os.system("pkg install -y ffmpeg")
-os.system("pip install -y youtube_dl")
+os.system("yes | pip install  yt-dlp")
 os.system("python setup.py install")
 input("Installation finished .....")
