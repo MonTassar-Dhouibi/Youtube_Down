eeb999b504d1c36f06704716f8b16af23472d4e4
VËndîtta0VËN0M <72995093+VEnditta0Xx@users.noreply.github.com>
2022-05-23 02:31:35 +0100

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -1,23 +1,48 @@
-
 from __future__ import unicode_literals
 import yt_dlp as youtube_dl
-import sys, os
+import sys
+import glob, os
+from mutagen.id3 import ID3, APIC, error,TALB
+import shutil ,random
+
+#path = "/storage/emulated/0/PMusic3/.tmp/"
 
 
-os.system("termux-wake-lock")
+def CImg():
+	aFile = glob.glob(("*.mp3"))[0]
+	tags = ID3(aFile)
+	try :
+		Name = aFile.split("-")[1].replace(".mp3","")
+		id = "YDown | " +Name+ str(random.randint(1,99))
+	except:
+		id = "YDown | "+ aFile.replace(".mp3","") +str(random.randint(1,99))
+	tags.add(TALB(encoding=3, text=id))
+	tags.save()
+	
+
+	
+	shutil.move(aFile,"/storage/emulated/0/PMusic3/"+aFile)
+
+	
+
 def main():
+	i= 0
+	path = "/storage/emulated/0/PMusic3/.tmp/"
 	try :
 		if len(sys.argv)>1:
  	     	  link = sys.argv[1]
 		else:
         		print("Insert the link")
         		link = input ("")
-
+		while os.path.isdir(path) :
+			i+=1 
+			path = "/storage/emulated/0/PMusic3/.tmp"+str(i) +"/"
 		ydl_opts = {
     'format': 'bestaudio/best',
+    '%(title)s': "gt",
     'writethumbnail': True,
     'add-metadata':True,
-     'outtmpl': '/storage/emulated/0/PMusic/%(title)s.%(ext)s',
+     'outtmpl': path+'%(title)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -29,10 +54,15 @@ def main():
 	}
 
 		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
- 		   	ydl.download([link])
-	except:
- 	   print("[+] 0 :EXIT\n[+] 1 :RETRY ")
- 	   c = int(input())
+			   	ydl.download([link])
+			   	
+			   	os.chdir(path)
+			   	CImg()
+			   	os.rmdir(path)
+ 		   	
+	except Exception as e:
+ 	   print(e,"\n[+] 0 :EXIT\n[+] 1 :RETRY ")
+ 	   c = int(input("-->  "))
  	   if c ==0:
  	   	sys.exit()
  	   else :
@@ -40,4 +70,4 @@ def main():
  	   
  	   
 if __name__ == "__main__":
-	main();sys.exit()
+	main()
